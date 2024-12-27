1067cdcf010d63a7f4e20072df15c7c63ce532cf
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-27 13:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let f_name=document.getElementById(first_name);
 let l_name=document.getElementById(last_name);
 let phone=document.getElementById(phone_no);
 let email=document.getElementById(email_id);
-fucntion Click(){
-	alert('First Name: ${f_name.value} Last Name: ${l_name.value} Phone Number: ${phone.value} Email ID: ${email.value}');
+function Click(){
+	alert(`First Name: ${f_name.value} Last Name: ${l_name.value} Phone Number: ${phone.value} Email ID: ${email.value}`);
 }
