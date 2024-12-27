1bf6374f03f293e23100975433906401edcc450f
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-27 13:11:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let f_name=document.getElementById(first_name);
+let l_name=document.getElementById(last_name);
+let phone=document.getElementById(phone_no);
+let email=document.getElementById(email_id);
+fucntion Click(){
+	alert('First Name: ${f_name.value} Last Name: ${l_name.value} Phone Number: ${phone.value} Email ID: ${email.value}');
+}
